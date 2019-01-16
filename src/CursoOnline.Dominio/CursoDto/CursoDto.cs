f7ce679fc9e5cdcf280a19eb2e7c9480e6abd47a
Douglas da Mata <douglasfigsouza@gmail.com>
2019-01-16 12:58:37 -0200

    commit inicial

---
+++ src/CursoOnline.Dominio/CursoDto/CursoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CursoOnline.Dominio.CursoDto
+{
+    public class CursoDto
+    {
+        public string Nome { get; set; }
+        public int CargaHoraria { get; set; }
+        public string PublicoAlvo { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
---
+++ test/CursoOnline.DominioTest/Cursos/CusoTest.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class CusoTest
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            var nome = "informatica";
+            int cargaHoraria = 80;
+            var publicoAlvo = "estudantes";
+            decimal valor = 950;
+
+            var curso = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+
+            Assert.Equal(nome, curso.Nome);
+            Assert.Equal(cargaHoraria, curso.CargaHoraria);
+            Assert.Equal(publicoAlvo, curso.PublicoAlvo);
+            Assert.Equal(valor, curso.Valor);
+
+
+        }
+    }
+
+    public class Curso
+    {
+        public Curso(string nome, int cargaHoraria, string publicoAlvo, decimal valor)
+        {
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+        }
+
+        public string Nome { get; set; }
+        public int CargaHoraria { get; set; }
+        public string PublicoAlvo { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
